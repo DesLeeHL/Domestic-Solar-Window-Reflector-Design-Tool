11ea99a84ab5db217ee4915ad2e6fda701e0b551
DesLeeHL <lih4@tcd.ie>
2023-03-02 23:36:21 +0000

    no error, but blank page

--- React-Project/solar-reflector-app/src/three-main/ThreeMain.jsx
+++ React-Project/solar-reflector-app/src/three-main/ThreeMain.jsx
@@ -1,13 +1,17 @@
-import React, { useRef, useState } from "react";
+import React, { useRef,useEffect, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, OrthographicCamera } from "@react-three/drei";
+// import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 import { DoubleSide } from 'three';
 import Slider from "../components/Slider"
 
-function ThreeMain(props) {
-    // This reference gives us direct access to the THREE.Mesh object
-    const ref = useRef();
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as THREE from "three";
+
+function ThreeObjects() {
+    // // This reference gives us direct access to the THREE.Mesh object
+    // const ref = useRef();
     // Hold state for hovered and clicked events
     const [width, setWidth] = useState(10);
     const [height, setHeight] = useState(15);
@@ -22,176 +26,170 @@ function ThreeMain(props) {
         top: 0,
         left: 0,
     };
+    const windowGeometry = new THREE.BoxGeometry(width, height, 0.5);
+    const windowMaterial = new THREE.MeshStandardMaterial({ wireframe: true, color: "orange" });
+    const windowMesh = new THREE.Mesh(windowGeometry, windowMaterial);
 
-    // const reflectorRotator=new THREE.Mesh({position:[0, -height / 2, 0], rotation:[-angle * (Math.PI / 180), 0, 0]})
 
-    // const reflectorMaterial = new THREE.MeshStandardMaterial({ side: THREE.DoubleSide });
-    // const reflectorGeometry = new THREE.PlaneGeometry(width, mirrorLength, 2);
-    // const reflector = new THREE.Mesh(reflectorGeometry, reflectorMaterial);
-    // reflector.position.set(0, mirrorLength / 2, 0);
-    // reflector.name = 'reflector';
-
-    // const sunlightMaterial = new THREE.MeshStandardMaterial({ color: 'white', opacity: 1, transparent: true, side: THREE.DoubleSide });
-    // const sunlightGeometry = new THREE.PlaneGeometry(width, mirrorLength, 2);
-    // const sunlight = new THREE.Mesh(sunlightGeometry, sunlightMaterial);
-    // sunlight.position.set(0, lightDistY, lightDistZ);
-    // sunlight.name = 'sunlight';
-
-    return (
-        <>
-
-            <Canvas style={canvasStyle}>
-                {/* <OrthographicCamera /> */}
-                <pointLight position={[10, 10, 10]} />
-                <ambientLight />
-                <mesh
-                    {...props}
-                    ref={ref}
-                >
-                    {/* WINDOW */}
-                    <boxGeometry args={[width, height, 0.5]} />
-                    <meshStandardMaterial
-                        wireframe={true}
-                        color={"orange"}
-                    />
-                    {/**Up Down indicator */}
-                    <mesh position={[0, 1, 0]}>
-                        <coneGeometry args={[0.2, 5, 3]} />
-                        <meshStandardMaterial />
-                    </mesh>
-
-                    {/* TODO: Perpendicular cone to indicate inside and outside
-                    <mesh position={[0, 0, 1]}>
-                        <coneGeometry args={[0.2, 5, 3]} />
-                        <meshStandardMaterial />
-                    </mesh> */}
-                    {/* REFLECTOR */}
-                    <mesh position={[0, -height / 2, 0]} rotation={[-angle * (Math.PI / 180), 0, 0]}>
-                        {/* REFLECTOR */}
-                        <mesh position={[0, mirrorLength / 2, 0]}>
-                            <planeGeometry args={[width, mirrorLength, 2]} />
-                            <meshStandardMaterial side={DoubleSide} />
-                        </mesh>
-                        {/* SYMMETRY PLANE */}
-                        <mesh position={[0, mirrorLength / 2, 0]} rotation={[Math.PI / 2, 0, 0]}>
-                            <planeGeometry args={[height, width, 2]} />
-                            <meshStandardMaterial color={"white"} opacity={0.2} transparent={true} side={DoubleSide} />
-                        </mesh>
-                        {/* PARALLEL PLANE FOR SUNLIGHT */}
-                        <mesh position={[0, lightDistY, lightDistZ]}>
-                            <planeGeometry args={[width, mirrorLength, 2]} />
-                            <meshStandardMaterial color={'white'} opacity={1} transparent={true} side={DoubleSide} />
-                        </mesh>
-
-                    </mesh>
-
-                </mesh>
-                <OrbitControls />
-
-                <OrthographicCamera
-                    makeDefault
-                    zoom={10}
-                    top={500}
-                    bottom={-500}
-                    left={500}
-                    right={-500}
-                    near={1}
-                    far={800}
-                    position={[10, 10, 100]}
-                />
-            </Canvas>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-                {/* <Slider
-                    label="Window Width:"
-                    min={1}
-                    max={20}
-                    val={width}
-                    funcOnChange={(e) => setWidth(e.target.value)}
-                /> */}
-                <div style={{ display: "flex", alignItems: "center" }}>
-                    <label htmlFor="width-slider">Window Width:</label>
-                    <input
-                        id="width-slider"
-                        type="range"
-                        min={1}
-                        max={20}
-                        value={width}
-                        onChange={e => setWidth(e.target.value)}
-                        style={{ marginLeft: "0.5rem", marginRight: "0.5rem" }}
-                    />
-                    <span>{width}</span>
-                </div>
-                <div style={{ display: "flex", alignItems: "center" }}>
-                    <label htmlFor="height-slider">Window Height:</label>
-                    <input
-                        id="height-slider"
-                        type="range"
-                        min={1}
-                        max={20}
-                        value={height}
-                        onChange={e => setHeight(e.target.value)}
-                        style={{ marginLeft: "0.5rem", marginRight: "0.5rem" }}
-                    />
-                    <span>{height}</span>
-                </div>
-                <div style={{ display: "flex", alignItems: "center" }}>
-                    <label htmlFor="height-slider">Reflector Angle:</label>
-                    <input
-                        id="height-slider"
-                        type="range"
-                        min={0}
-                        max={180}
-                        value={angle}
-                        onChange={e => setAngle(e.target.value)}
-                        style={{ marginLeft: "0.5rem", marginRight: "0.5rem" }}
-                    />
-                    <span>{angle}</span>
-                </div>
-                <div style={{ display: "flex", alignItems: "center" }}>
-                    <label htmlFor="height-slider">Reflecor Length:</label>
-                    <input
-                        id="height-slider"
-                        type="range"
-                        min={0}
-                        max={height}
-                        value={mirrorLength}
-                        onChange={e => setMirrorLength(e.target.value)}
-                        style={{ marginLeft: "0.5rem", marginRight: "0.5rem" }}
-                    />
-                    <span>{mirrorLength}</span>
-                </div>
-                <div style={{ display: "flex", alignItems: "center" }}>
-                    <label htmlFor="height-slider">Y Distance of Light Simulation Plane:</label>
-                    <input
-                        id="height-slider"
-                        type="range"
-                        min={0}
-                        max={50}
-                        value={lightDistY}
-                        onChange={e => setLightDistY(e.target.value)}
-                        style={{ marginLeft: "0.5rem", marginRight: "0.5rem" }}
-                    />
-                    <span>{lightDistY}</span>
-                </div>
-                <div style={{ display: "flex", alignItems: "center" }}>
-                    <label htmlFor="height-slider">Z Distance of Light Simulation Plane:</label>
-                    <input
-                        id="height-slider"
-                        type="range"
-                        min={0}
-                        max={50}
-                        value={lightDistZ}
-                        onChange={e => setLightDistZ(e.target.value)}
-                        style={{ marginLeft: "0.5rem", marginRight: "0.5rem" }}
-                    />
-                    <span>{lightDistZ}</span>
-                </div>
-                {/* <div style={{ display: "flex", alignItems: "center" }}>
-                    Zoom: 
-                </div> */}
-            </div>
-        </>
+    const reflectorMesh = new THREE.Mesh(
+        new THREE.PlaneGeometry(width, mirrorLength, 2),
+        new THREE.MeshStandardMaterial({ side: THREE.DoubleSide })
     );
+    reflectorMesh.position.set(0, mirrorLength / 2, 0);
+
+    const symmetryPlaneMesh = new THREE.Mesh(
+        new THREE.PlaneGeometry(height, width, 2),
+        new THREE.MeshStandardMaterial({
+            color: "white",
+            opacity: 0.2,
+            transparent: true,
+            side: THREE.DoubleSide,
+        })
+    );
+    symmetryPlaneMesh.position.set(0, mirrorLength / 2, 0);
+    symmetryPlaneMesh.rotation.set(Math.PI / 2, 0, 0);
+
+    const parallelPlaneMesh = new THREE.Mesh(
+        new THREE.PlaneGeometry(width, mirrorLength, 2),
+        new THREE.MeshStandardMaterial({
+            color: "white",
+            opacity: 1,
+            transparent: true,
+            side: THREE.DoubleSide,
+        })
+    );
+    parallelPlaneMesh.position.set(0, lightDistY, lightDistZ);
+
+    const reflectorGroup = new THREE.Group();
+    reflectorGroup.add(reflectorMesh, symmetryPlaneMesh, parallelPlaneMesh);
+
+    const reflector = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshStandardMaterial({ wireframe: true, color: "orange" })
+    );
+    reflectorGroup.position.setY(-height / 2);
+    reflectorGroup.rotation.setX(-angle * (Math.PI / 180));
+    return {windowMesh, reflectorGroup}
 }
 
-export default ThreeMain;
+const {windowMesh, reflectorGroup} = ThreeObjects();
+function easeOutCirc(x) {
+    return Math.sqrt(1 - Math.pow(x - 1, 4));
+  }
+
+const TheScene = () => {
+    const refContainer = useRef();
+    const [loading, setLoading] = useState(true);
+    const [renderer, setRenderer] = useState();
+
+    useEffect(() => {
+        const { current: container } = refContainer;
+        if (container && !renderer) {
+            const scW = container.clientWidth;
+            const scH = container.clientHeight;
+            const renderer = new THREE.WebGLRenderer({
+                antialias: true,
+                alpha: true
+            });
+            renderer.setPixelRatio(window.devicePixelRatio);
+            renderer.setSize(scW, scH);
+            renderer.outputEncoding = THREE.sRGBEncoding;
+            container.appendChild(renderer.domElement);
+            setRenderer(renderer);
+
+            const scene = new THREE.Scene();
+            const scale = 5.6;
+            const camera = new THREE.OrthographicCamera(
+                -scale,
+                scale,
+                scale,
+                -scale,
+                0.01,
+                50000
+            );
+            const target = new THREE.Vector3(-0.5, 1.2, 0);
+            const initialCameraPosition = new THREE.Vector3(
+                20 * Math.sin(0.2 * Math.PI),
+                10,
+                20 * Math.cos(0.2 * Math.PI)
+            );
+            const ambientLight = new THREE.AmbientLight(0xcccccc, 1);
+            scene.add(ambientLight);
+            const controls = new OrbitControls(camera, renderer.domElement);
+            controls.autoRotate = true;
+            controls.target = target;
+
+            scene.add(reflectorGroup).then(() => {
+                animate();
+                setLoading(false);
+            });
+
+            let req = null;
+            let frame = 0;
+            const animate = () => {
+                req = requestAnimationFrame(animate);
+                frame = frame <= 100 ? frame + 1 : frame;
+
+                if (frame <= 100) {
+                    const p = initialCameraPosition;
+                    const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20;
+
+                    camera.position.y = 10;
+                    camera.position.x =
+                        p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed);
+                    camera.position.z =
+                        p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed);
+                    camera.lookAt(target);
+                } else {
+                    controls.update();
+                }
+
+                renderer.render(scene, camera);
+            };
+
+            return () => {
+                cancelAnimationFrame(req);
+                renderer.dispose();
+            };
+        }
+    }, []);
+
+    return (
+        <div
+            style={{ height: "540px", width: "540px", position: "relative" }}
+            ref={refContainer}
+        >
+            {loading && (
+                <span style={{ position: "absolute", left: "50%", top: "50%" }}>
+                    Loading...
+                </span>
+            )}
+        </div>
+    );
+}
+// const reflectorRotator=new THREE.Mesh({position:[0, -height / 2, 0], rotation:[-angle * (Math.PI / 180), 0, 0]})
+
+// const reflectorMaterial = new THREE.MeshStandardMaterial({ side: THREE.DoubleSide });
+// const reflectorGeometry = new THREE.PlaneGeometry(width, mirrorLength, 2);
+// const reflector = new THREE.Mesh(reflectorGeometry, reflectorMaterial);
+// reflector.position.set(0, mirrorLength / 2, 0);
+// reflector.name = 'reflector';
+
+// const sunlightMaterial = new THREE.MeshStandardMaterial({ color: 'white', opacity: 1, transparent: true, side: THREE.DoubleSide });
+// const sunlightGeometry = new THREE.PlaneGeometry(width, mirrorLength, 2);
+// const sunlight = new THREE.Mesh(sunlightGeometry, sunlightMaterial);
+// sunlight.position.set(0, lightDistY, lightDistZ);
+// sunlight.name = 'sunlight';
+
+
+export default function ThreeMain(){
+    return(
+        <div>
+            <p>Click and hold to move around</p>
+      <p>
+       Lorem ipsum.
+      </p>
+      <TheScene />
+        </div>
+    )
+}
